a0b55a43f4f525e5c1d9060c8e44e08f7657551d
tigran <t.kostandyan1@gmail.com>
2018-10-24 00:00:38 +0300

    Final version

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mandelbrot set 
+
+<p align="center">
+    <img src="https://i.imgur.com/fVeOUcX.png">
+    </br>
+</p>
+Simple mandelbrot set implementation in JS.
--- mandelbrot.js
+++ mandelbrot.js
@@ -1,4 +1,5 @@
-const MAXITER = 100;
+const MAXITER = 100
+
 
 let Complex = class {
     constructor(real, imaginary) {
@@ -6,41 +7,54 @@ let Complex = class {
         this.imaginary = imaginary
     }
     pow() {
-        let real = this.real ** 2 - this.imaginary
-        let imaginary = 2 * this.real * this.imaginary
+        let real = this.real ** 2.0 - this.imaginary ** 2.0
+        let imaginary = 2.0 * this.real * this.imaginary
         return new Complex(real, imaginary)
     }
     abs() {
-        return this.real ** 2 + this.imaginary ** 2 // should be a square root of expression,
+        return (this.real ** 2.0 + this.imaginary ** 2.0) // should be a square root of expression,
                                                     // but in order to speed this up a little bit, we will use it like this
     }
     add(r, i) {
         let real = this.real + r
         let imaginary = this.imaginary + i
-        return new Complex(real, this.imaginary)
+        return new Complex(real, imaginary)
     }
 }
-let mandelbrot = (z, MAXITER) => {
+
+let mandelbrot = (z) => {
     let c = z
-    for (i = 0; i < MAXITER; i++) {
-        if (c.abs() > 4) {
+    for (let i = 0; i < MAXITER; i++) {
+        if (z.abs() > 4) {
             return i
         }
-        z = z.pow().add(c.real, c.imaginary) 
+        z = z.pow().add(c.real, c.imaginary)
     }
+    return 0
 }
 
-let mandelbrot_set = (canvas, xmin, xmax, ymin, ymax, MAXITER) => {
+let mandelbrot_set = (canvas) => {
     let ctx = canvas.getContext('2d')
-    width = canvas.width
-    height = canvas.height
-    let img = ctx.createImage(0, 0, width, height)
-    let px = img.data
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-
+    ctx.canvas.width = window.innerWidth
+    ctx.canvas.height = window.innerHeight
+    let img = ctx.createImageData(ctx.canvas.width, ctx.canvas.height)
+    for (let x = 0; x < ctx.canvas.width; x++) {
+        for (let y = 0; y < ctx.canvas.height; y++) {
+            let cx = -2 + x/300
+            let cy = -2 + y/300
+            let i = mandelbrot(new Complex(cx, cy))
+            if (i === 0) {
+                ctx.fillStyle = '#000'
+                ctx.fillRect(x, y, 1, 1)
+            } else {
+                ctx.fillStyle = `hsl(0, 100%, ${i}%)`
+                ctx.fillRect(x, y, 1, 1)
+            }
+           
         }
     }
-    
+
 }
+
 let canvas = document.getElementById('canvasMandelbrot')
+mandelbrot_set(canvas)
